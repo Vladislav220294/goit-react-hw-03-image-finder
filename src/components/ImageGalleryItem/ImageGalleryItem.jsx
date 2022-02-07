0f5de300e3eac3c03065c40f04dd2baf96c2c728
Vladislav220294 <vladislav22021994@gmail.com>
2022-02-08 01:16:27 +0300

    add scroll, but the issue with total render page is unsolved

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,11 +1,23 @@
 import PropTypes from 'prop-types';
-import Modal from '../Modal/Modal'
-import s from './/ImageGalleryItem.module.css'
+import s from './/ImageGalleryItem.module.css';
 
-const ImageGalleryItem = ({toggleModal, showModal, id, webformatURL, largeImageURL}) => {
-    return ( <><li key={id} className={s.imageGalleryItem}>
-  <img src={webformatURL} alt="" className={s.imageGalleryItemimage} /> 
-</li> {showModal && <Modal largeImageURL={largeImageURL} toggleModal={toggleModal} />} </>);
-}
- 
+const ImageGalleryItem = ({ toggleModal, webformatURL, largeImageURL }) => {
+  return (
+    <>
+      <li className={s.imageGalleryItem}>
+        <img
+          src={webformatURL}
+          alt=""
+          className={s.imageGalleryItemimage}
+          onClick={() => toggleModal(largeImageURL)}
+        />
+      </li>{' '}
+    </>
+  );
+};
+ImageGalleryItem.propTypes = {
+  toggleModal: PropTypes.func.isRequired,
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+};
 export default ImageGalleryItem;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,41 +1,47 @@
 import PropTypes from 'prop-types';
 import { Component } from 'react/cjs/react.production.min';
+import s from '../Searchbar/Searchbar.module.css';
 
 class Searchbar extends Component {
-    state = {
+  state = {
     inputalue: '',
+  };
+  onInputValue = e => {
+    this.setState({ inputalue: e.currentTarget.value.toLowerCase().trim() });
+  };
+  onImagesSubmit = e => {
+    const { SubmitForm } = this.props;
+    const { inputalue } = this.state;
+    e.preventDefault();
+    if (inputalue.trim() === '') {
+      return alert('wrong name');
     }
-    onInputValue = e => {
-        this.setState({inputalue : e.currentTarget.value.toLowerCase().trim() });
-    }
-    onImagesSubmit = e => {
-        const { SubmitForm } = this.props;
-        const { inputalue } = this.state;
-        e.preventDefault();
-      if (inputalue.trim() === '') { return alert('wrong name') };
-      SubmitForm(inputalue);
-        this.setState({ inputalue: "" });
-    }
-    render() {
-        return (<header class="searchbar">
-  <form class="form" onSubmit={this.onImagesSubmit}>
-    <button type="submit" class="button">
-      <span class="button-label">Search</span>
-    </button>
+    SubmitForm(inputalue);
+    this.setState({ inputalue: '' });
+  };
+  render() {
+    return (
+      <header className={s.searchbar}>
+        <form onSubmit={this.onImagesSubmit} className={s.searchForm}>
+          <button type="submit" className={s.searchFormbutton}>
+            <span className={s.searchFormbuttonlabel}>Search</span>
+          </button>
 
-                <input
-    onChange={this.onInputValue}
-    value={this.state.inputalue}
-      class="input"
-      type="text"
-      autoComplete="off"
-      autoFocus
-      placeholder="Search images and photos"
-    />
-  </form>
-</header> );
-    }
-    
+          <input
+            onChange={this.onInputValue}
+            value={this.state.inputalue}
+            className={s.searchForminput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
 }
- 
+Searchbar.propTypes = {
+  SubmitForm: PropTypes.func.isRequired,
+};
 export default Searchbar;
